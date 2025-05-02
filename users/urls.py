0aae49809dbe059daec6e982c2639191fc6f1e9f
Vasudeva8278 <111454632+Vasudeva8278@users.noreply.github.com>
2025-05-02 11:40:59 +0530

    commit

--- users/urls.py
+++ users/urls.py
@@ -8,3 +8,13 @@ urlpatterns = [
     path('register/', views.CustomSignupView.as_view(), name='register'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
 ]
+
+# filepath: c:\Users\hp\Desktop\pro\Daily_Attaks_New\users\templatetags\form_filters.py
+from django import template
+
+register = template.Library()
+
+@register.filter
+def example_filter(value):
+    # Example filter logic
+    return value.upper()
